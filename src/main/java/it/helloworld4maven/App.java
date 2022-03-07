5995be29bfe7b5cbf5b0bd59d74875ebe471bbd9
cops <cops@debian.debian.tagete.com>
2022-03-07 10:27:25 -0500

    Initial commit

---
+++ src/main/java/it/helloworld4maven/App.java
@@ -0,0 +1,13 @@
+package it.helloworld4maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
